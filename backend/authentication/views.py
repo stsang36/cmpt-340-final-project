51dd4e4fa64995c705a438b8eb85b5054734e4af
Sean Wotherspoon <98927896+Seanspoons@users.noreply.github.com>
2024-03-15 22:43:16 -0700

    Fixed import statement for serializers in authenticaiton app

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -9,7 +9,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.authtoken.models import Token
-from backend.authentication.serializers import UserSerializer
+from authentication.serializers import UserSerializer
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
